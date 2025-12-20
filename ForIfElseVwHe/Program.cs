26bc3072fac51c4d233bec94b5f1f2f167826059
dasss151234 <sevaturaev13@gmail.com>
2025-12-20 13:02:25 +0300

    25.12.18

---
+++ ForIfElseVwHe/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace ForIfElseVwHe
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> list = new List<string>();
+            string a = "чё";
+            list.Add(a);
+            list.Add("где");
+            list.Add("когда");
+            list.Add("?");
+            //list.Remove(a);
+            //Console.WriteLine(list[0]);
+            for (int i = 0; i < list.Count; i = i + 2)
+            {
+                Console.Write($"{list[i]} ");
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
