0b0ec19b3945aaab32811b11f07039f869567c0f
dasss151234 <sevaturaev13@gmail.com>
2025-12-20 20:43:05 +0300

    25.12.20dz1
    ура я его сделал!!!

---
+++ dz1/Program.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections.Generic;
+using System.IO;
+namespace dz1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            1
+            int apples = 7;
+            Console.WriteLine("У меня есть {0} яблок.", apples);
+            
+            1.2
+            int a = 57;
+            int b = 10;
+            Console.WriteLine(a + b);
+            
+            2
+            double price = 12.5;
+            string s = "хлеб";
+            Console.WriteLine("за {0} надо отдать денег {1} рублей.", s, price);
+            
+            2.2
+            double a = 33.4;
+            double b = 33.6;
+            Console.WriteLine(a + b);
+            
+            3
+            bool isSunny = true;
+            Console.WriteLine("Сегодня солнечный день? {0}", isSunny);
+            
+            3.2
+            int age = 15;
+            int isAdult = false;
+            if (age >= 18)
+            {
+               isAdult = true;
+            }
+            
+            4
+            char firstLetter = 'A';
+            Console.WriteLine("{0} B C D I F G...", firstLetter);
+            
+            4.2
+            char grade = '5';
+            Console.WriteLine("Моя оценка: {0}", grade);
+            
+            5
+            string name = "Макс";
+            Console.WriteLine("Пр {0}. Го в фортнайт?", name);
+            
+            5.2
+            string name = "Макс";
+            string name2 = "Давай";
+            Console.WriteLine("{0} пойдешь играть в фортнайт? МАКС ОТВЕЧАЕТ - {1} .", name, name2);
+            
+            6
+            string[] fruits = { "яблоко", "банан", "голубика" };
+            Console.WriteLine(fruits[0]);
+            
+            6.2
+            string[] numders = { "67", "25", "52", "666", "12"};
+            Console.WriteLine(numders[4]);
+            
+            
+            7
+            string[] tovaru = new string[0003];
+            tovaru[0] = "чипсы";
+            //tovaru[1] = "кола";
+            tovaru[2] = "йогурт";
+            for (int i = 0; i < tovaru.Length; i++)
+            {
+                Console.WriteLine(tovaru[i]);
+            }
+            
+            8
+            List<string> numbers = new List<string>();
+            numbers.Add("1");
+            numbers.Add("2");
+            numbers.Add("3");
+            numbers.Add("4");
+            numbers.Add("5");
+            numbers.Add("6");
+            numbers.Add("7");
+            numbers.Add("8");
+            numbers.Add("9");
+            numbers.Add("10");
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                Console.WriteLine($"{numbers[i]} ");
+            }
+            
+            //9
+            string[] animal = { "жираф", "свинья", "кот", "жаба" };
+            for (int i = 0; i < animal.Length; i++)
+            {
+                Console.WriteLine(animal[i]);
+            }
+             */
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
