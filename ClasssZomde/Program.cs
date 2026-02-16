f7c6a80b3d04b29bb90eaf06fa6398fe02a8241b
dasss151234 <sevaturaev13@gmail.com>
2026-02-16 20:57:34 +0300

    26.02.16
    hgbv ghvj

---
+++ ClasssZomde/Program.cs
@@ -0,0 +1,65 @@
+﻿namespace ClasssZomde
+{
+    class Zombie
+    {
+        public int Health;
+        public int Damage;
+        public string Name;
+        public Zombie (string name, int health, int damage) 
+        {
+            Name = name;
+            Health = health;
+            Damage = damage;
+        }
+        public void TakeDamage(int amount)
+        {
+            Health -= amount;
+            Console.WriteLine(Name + " получил " + amount + " урона");
+            Console.WriteLine("Осталось HP: " + Health);
+        }
+        public void Attack()
+        {
+            Console.WriteLine(Name + " атакует и наносит " + Damage + " урона");
+        }
+        public bool IsAlive()
+        {
+            return Health > 0;
+        }
+
+    }
+
+    class Player
+    {
+        public int Health;
+        public int Damage;
+        public string Name;
+        public Player(string name, int health, int damage)
+        {
+            Name = name;
+            Health = health;
+            Damage = damage;
+        }
+        public void Attack(Zombie zzzzzooooo)
+        {
+            zzzzzooooo.TakeDamage(Damage);
+        }
+        public bool IsAlive()
+        {
+            return Health > 0;
+        }
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Zombie zzzzz = new Zombie("lox2", 100, 15);
+            Player player = new Player("lox1", 100, 30);
+            while (player.IsAlive() && zzzzz.IsAlive() ) 
+            {
+                player.Attack(zzzzz);
+            }
+
+        }
+    }
+}
