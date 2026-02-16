7a4d89f3d8ca415ab4d1224443e8ca3b79a0e2d0
dasss151234 <sevaturaev13@gmail.com>
2026-02-16 19:38:37 +0300

    26.02.16
    uygukhyv

---
+++ classes/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace classes
+{
+    class PlayerBob
+    {
+        public string Name;
+        public int Health;
+        public int Damage;
+        public void Attack()
+        {
+            Console.WriteLine($"{Name} атакует и наносит {Damage} урона!");
+        }
+        public void TakeDamage(int amount)
+        {
+            Health -= amount;
+            Console.WriteLine($"{Name} получил {amount} урона!");
+        }
+        public bool IsAlive()
+        {
+            return Health > 0;
+        }
+
+    }
+    internal class Programloooooool
+    {
+        static void Main(string[] args)
+        {
+            PlayerBob player = new PlayerBob();
+            PlayerBob ugrok = new PlayerBob();
+            ugrok.Name = "8fujisoul";
+            ugrok.Health = 100;
+            ugrok.Damage = 11;
+            player.Name = "Seva";
+            player.Health = 100;
+            player.Damage = 11;
+            Console.WriteLine(player.Name);
+            Console.WriteLine(player.Health);
+            Console.WriteLine(player.Damage);
+            ugrok.Attack();
+            player.TakeDamage(ugrok.Damage);
+            if (player.IsAlive())
+            
+                Console.WriteLine("Игрок жив");
+            else
+                Console.WriteLine("Игрок мёртв");
+
+            
+        }
+    }
+}
